f34626dfec7e6ed170ccde60fd8e7c612b5145c5
Tural Guluev <tural-98@bk.ru>
2022-11-17 20:09:43 +0400

    tests are completed

--- src/test/java/ru/guluev/MySpringBootApplicationTests.java
+++ src/test/java/ru/guluev/MySpringBootApplicationTests.java
@@ -1,13 +1,43 @@
 package ru.guluev;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MySpringBootApplicationTests {
 
+    public static GenericContainer<?> firstContainer = new GenericContainer<>("devapp");
+    public static GenericContainer<?> secondContainer = new GenericContainer<>("prodapp");
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @BeforeAll
+    public static void setUp() {
+        firstContainer.start();
+        secondContainer.start();
+    }
+
     @Test
-    void contextLoads() {
+    void firstImage() {
+        int first = firstContainer.getMappedPort(8080);
+
+        ResponseEntity<?> responseEntity = restTemplate.getForEntity("http://localhost:" + first + "/profile", String.class);
+        System.out.println(responseEntity.getBody());
+    }
+
+    @Test
+    void secondImage() {
+        int second = secondContainer.getMappedPort(8080);
+
+        ResponseEntity<?> responseEntity = restTemplate.getForEntity("http://localhost:" + second + "/profile", String.class);
+
+        System.out.println(responseEntity.getBody());
     }
 
 }
